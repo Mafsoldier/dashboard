a9940ce52c2d5be8b37731075d9598da28cc48d9
Marc Willemsen <marc@bolt.nl>
2019-12-23 19:17:50 +0100

    connection database, connection css file

---
+++ main/index.php
@@ -0,0 +1,49 @@
+<?php 
+
+require_once("../db_config.php");
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css" type="text/css">
+</head>
+
+
+<body>
+
+<header>
+
+    <p> This is the header</p>
+
+ </header>
+
+
+
+<main>
+
+    <p> This is the main page</p>
+
+ </main>
+
+<footer> 
+
+<p> This is the footer page</p>
+
+</footer>
+
+
+    
+</body>
+
+
+
+
+
+</html>
