bd0b506a7b3b9e30ec9dd5f531c4607b3d789aca
Marc Willemsen <marc@bolt.nl>
2019-12-24 14:51:06 +0100

    - Grid layout bepaald

--- main/index.php
+++ main/index.php
@@ -20,16 +20,102 @@ require_once("../db_config.php");
 
  <div class="containerheader">
 
-    <p> This is the header</p>
+    <div class="headeritem1">
+        <p> This is the header</p>
+    </div>
+    <div class="headeritem2">
+        <p> This is the header</p>
+    </div>
+    <div class="headeritem3">
+        <p> This is the header</p>
+    </div>
+
+
+    
 
 </div>
+
+
+
+
+
+
+
+
+
 
 
 <div class="containercontent">
 
-    <p> This is the main page</p>
+    <div class="item1">
+
+        
+    </div>
+
+
+    <div class="heroName1">
+
+    </div>
+
+    <div class= "heroName2">
+
+    </div>
+
+    <div class="scoreCounter">
+
+    </div>
+
+            
+    <div class="heroName1 grid-item">
+        <p> Hero name 1 </p>    
+    </div>
+
+    <div class="itemPlayer1">
+
+        <!-- <p> PLAYER 1</p>     -->
+        <img src="superman.jpg" alt="Player 1">
+
+    </div>
+
+    <div class="itemVS grid-item">
+
+         <p> VS</p>
+
+    </div>  
+
+    <div class="heroName2 grid-item">
+        <p> Hero name 2 </p>    
+    </div>
+    
+    <div class="itemPlayer2">
+
+        <!-- <p> PLAYER 2</p>     -->
+        <img src="spiderman.jpg" alt="Player 2">
+
+    </div>
+
+    <div class="item5">
+
+        
+
+    </div>  
 
 </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 <footer class="footer"> 
 
