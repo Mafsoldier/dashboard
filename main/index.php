2b738b6bf1321d7dd44aa60c2e9bc9e412db9591
Marc Willemsen <marc@bolt.nl>
2019-12-31 13:58:44 +0100

    -reset knop geactiveerd -herpositionering van win of verlies signaal -herpositionering van reset knop

--- main/index.php
+++ main/index.php
@@ -11,22 +11,28 @@
 
     $is_page_refreshed = (isset($_SERVER['HTTP_CACHE_CONTROL']) && $_SERVER['HTTP_CACHE_CONTROL'] == 'max-age=0');
     
-    if($is_page_refreshed) {
-        session_unset();
+    if (!isset($_SESSION['playerWins']) || !isset($_SESSION['pcWins'])){
         $_SESSION['playerWins'] = 0;
-        $_SESSION['pcWins'] = 0;
-    } 
-
-    if (!isset($_SESSION['playerWins'])){
-        $_SESSION['playerWins'] = 0;
-    }
-
-    if (!isset($_SESSION['pcWins'])){
         $_SESSION['pcWins'] = 0;
     }
 
-
+   
     if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+
+        if ($is_page_refreshed){
+
+            $whoWins = " ";
+            $_SESSION['playerWins'] = 0;
+            $_SESSION['pcWins'] = 0;
+
+            
+        }elseif ($_POST['value'] == 'reset'){
+            
+            $whoWins = " ";
+            $_SESSION['playerWins'] = 0;
+            $_SESSION['pcWins'] = 0;
+
+        }else {
 
         if ($_POST['value'] == 1) {
             if($_POST['winner'] == 1){
@@ -41,20 +47,17 @@
             if($_POST['winner'] == 2){
                 $whoWins = "You've won!";
                 $_SESSION['playerWins']++;
+            
             }else {
-        
                 $whoWins = "You've lost!";
                 $_SESSION['pcWins']++;
             }
-            
+
         }else {
             $whoWins = "Wow! it was a draw!";
             }
-    }else {
-        $whoWins = " ";
-        $_SESSION['playerWins'] = 0;
-        $_SESSION['pcWins'] = 0;
     }
+}
 ?>
 
 <!DOCTYPE html>
@@ -73,7 +76,7 @@
 
     <div class="containerHeader">
         <div class="headerItem">
-        <h1> You've lost: <?php echo $_SESSION['pcWins'];?> times! </h1>
+            <h1> You've lost: <?php echo $_SESSION['pcWins'];?> times! </h1>
         </div>
         <div class="headerItem">
             <h1> WHO WINS? </h1>
@@ -92,36 +95,54 @@
         $count = 0;
         foreach($superheroes as $superHero) { 
             $count++;
-    
+            if ($count == 1){  
         ?>
         
         <form class="heroBox" action="#" method="post">  
-            <!-- <div class="heroBox itemContainerContent"> -->
-                <div class="itemHeroBox">
+            <div class="itemHeroBox">
                 <input class="button" type="submit" value="Ik kies <?php echo $superHero['name'];?>"> 
+            </div> 
+            <div class="itemHeroBox">
+                <input type="hidden" name='value'; value= <?php echo $count; ?>>  
+                <input type="hidden" name='winner'; value= <?php echo $result; ?>> 
+                <img class="img" src= <?php echo $superHero['image']; ?> alt="No image">  </img>    
+            </div>     
+
+        </form> 
+        
+        <form action='#' method='post'> 
+            <div class="itemHeroBox whoWin"> 
+                
+                <input type="hidden" name='value' value='reset'> 
+                <input class="button" type="submit" value="Reset">
+                <h1> <?php echo $whoWins; ?> </h1>  
+            </div>
+        </form>
+
+        <?php
+            }else { 
+        ?>
+            <form class="heroBox" action="#" method="post">  
+                <!-- <div class="heroBox itemContainerContent"> -->
+                <div class="itemHeroBox">
+                    <input class="button" type="submit" value="Ik kies <?php echo $superHero['name'];?>"> 
                 </div> 
                 <div class="itemHeroBox">
-                            <!--call superhero image and make it a button to submit-->
+                                <!--call superhero image and make it a button to submit-->
                     <input type="hidden" name='value'; value= <?php echo $count; ?>>  
                     <input type="hidden" name='winner'; value= <?php echo $result; ?>> 
-                    <img class="img" src= <?php echo $superHero['image']; ?> alt="No image">  </img>    
+                        <img class="img" src= <?php echo $superHero['image']; ?> alt="No image">  </img>    
                 </div>     
-                <div class="itemHeroBox">    
-                           
-                </div>
-        </form> 
-        <?php  }; ?>
+   
+            </form> 
+
+        <?php  
+            }
+
+         };
+        
+        ?>
     </div>
-
-    
-    <form action='#' method='post'> 
-    <div class="itemHeroBox whoWin"> <h1> <?php echo $whoWins; ?> </h1> </div>
-    <input type="hidden" name='reset' value='reset'> 
-    <input class="button" type="submit" value="START OVER"> 
-    </form>
-    
-    
-
 
 </body>
 </html>
